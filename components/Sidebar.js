0bcf464ec8fbaa9c2afccc1053ffa6693fcaff55
Annay Paul <annay.paul@g.bracu.ac.bd>
2023-07-10 15:23:13 +0600

    Created the  custom hook

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
     HomeIcon,
     SearchIcon,
@@ -6,93 +6,92 @@ import {
     PlusCircleIcon,
     HeartIcon,
     RssIcon,
-    LogoutIcon
+    LogoutIcon,
 } from "@heroicons/react/outline";
-import { signOut, useSession } from 'next-auth/react';
+import { signOut, useSession } from "next-auth/react";
 
 function Sidebar() {
     const { data: session, status } = useSession();
-    console.log(session);
-  return (
-    <div className='text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide'>
-        <div className="space-y-4">
-            <button 
-                className='flex items-center space-x-2 hover:text-white'
-                onClick={() => signOut()}
-            >
-                <LogoutIcon className='h-5 w-5' />
-                <p>Logout</p>
-            </button>
-            <button className='flex items-center space-x-2 hover:text-white'>
-                <HomeIcon className='h-5 w-5' />
-                <p>Home</p>
-            </button>
-            <button className='flex items-center space-x-2 hover:text-white'>
-                <SearchIcon className='h-5 w-5' />
-                <p>Search</p>
-            </button>
-            <button className='flex items-center space-x-2 hover:text-white'>
-                <LibraryIcon className='h-5 w-5' />
-                <p>Your Library</p>
-            </button>
-            <hr className='border-t-[0.1px] border-gray-900'/> 
 
-            <button className='flex items-center space-x-2 hover:text-white'>
-                <PlusCircleIcon className='h-5 w-5' />
-                <p>Create Playlist</p>
-            </button>
-            <button className='flex items-center space-x-2 hover:text-white'>
-                <HeartIcon className='h-5 w-5' />
-                <p>Liked Songs</p>
-            </button>
-            <button className='flex items-center space-x-2 hover:text-white'>
-                <RssIcon className='h-5 w-5' />
-                <p>Your Episodes</p>
-            </button>
-            <hr className='border-t-[0.1px] border-gray-900'/> 
-        {/* Playlist */}
-        <p className='cursor-pointer hover:text-white'>
-            Playlist Name..
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-            Playlist Name..
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-            Playlist Name..
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-            Playlist Name..
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-            Playlist Name..
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-            Playlist Name..
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-            Playlist Name..
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-            Playlist Name..
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-            Playlist Name..
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-            Playlist Name..
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-            Playlist Name..
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-            Playlist Name..
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-            Playlist Name..
-        </p>
+    return (
+        <div className='text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll h-screen scrollbar-hide'>
+            <div className='space-y-4'>
+                <button
+                    className='flex items-center space-x-2 hover:text-white'
+                    onClick={() => signOut()}>
+                    <LogoutIcon className='h-5 w-5' />
+                    <p>Logout</p>
+                </button>
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <HomeIcon className='h-5 w-5' />
+                    <p>Home</p>
+                </button>
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <SearchIcon className='h-5 w-5' />
+                    <p>Search</p>
+                </button>
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <LibraryIcon className='h-5 w-5' />
+                    <p>Your Library</p>
+                </button>
+                <hr className='border-t-[0.1px] border-gray-900' />
+
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <PlusCircleIcon className='h-5 w-5' />
+                    <p>Create Playlist</p>
+                </button>
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <HeartIcon className='h-5 w-5' />
+                    <p>Liked Songs</p>
+                </button>
+                <button className='flex items-center space-x-2 hover:text-white'>
+                    <RssIcon className='h-5 w-5' />
+                    <p>Your Episodes</p>
+                </button>
+                <hr className='border-t-[0.1px] border-gray-900' />
+                {/* Playlist */}
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist Name..
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist Name..
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist Name..
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist Name..
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist Name..
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist Name..
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist Name..
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist Name..
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist Name..
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist Name..
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist Name..
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist Name..
+                </p>
+                <p className='cursor-pointer hover:text-white'>
+                    Playlist Name..
+                </p>
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default Sidebar
+export default Sidebar;

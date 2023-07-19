2b0bb68d4fe994ecc12e8a149afeb947cf23d30b
Annay Paul <annay.paul@g.bracu.ac.bd>
2023-07-19 15:10:04 +0600

    Making responsive style changes to the Sidebar, Center, header and setting up the Songs component

--- components/Center.js
+++ components/Center.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import { ChevronDownIcon } from "@heroicons/react/outline";
 import { shuffle } from "lodash";
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { playlistIdState, playlistState } from "../atoms/playlistAtom";
+import useSpotify from "../hooks/useSpotify";
+import Songs from "./Songs";
 
 const colors = [
     "from-indigo-500",
@@ -18,12 +20,23 @@ const colors = [
 function Center() {
     const { data: session } = useSession();
     const [color, setColor] = useState(null);
+    const spotifyApi = useSpotify();
     const playlistId = useRecoilValue(playlistIdState);
     const [playlist, setPlaylist] = useRecoilState(playlistState);
 
     useEffect(() => {
         setColor(shuffle(colors).pop());
     }, [playlistId]);
+
+    useEffect(() => {
+        spotifyApi
+            .getPlaylist(playlistId)
+            .then((data) => setPlaylist(data.body))
+            .catch((err) =>
+                console.log("Something went wrong in fetching playlist", err)
+            );
+    }, [spotifyApi, playlistId]);
+    console.log(playlist);
 
     return (
         <div className='flex-grow'>
@@ -39,7 +52,22 @@ function Center() {
                 </div>
             </header>
             <section
-                className={`flex items-end space-x-7 bg-gradient-to-b to-black text-white ${color} h-80 text-white p-8`}></section>
+                className={`flex items-end space-x-7 bg-gradient-to-b to-black text-white ${color} h-80 text-white p-8`}>
+                <img
+                    className='h-44 w-44 shadow-2xl'
+                    src={playlist?.images?.[0]?.url}
+                    alt=''
+                />
+                <div>
+                    <p>PLAYLIST</p>
+                    <h1 className='text-2xl md:text-3xl xl:text-5xl font-bold'>
+                        {playlist?.name}
+                    </h1>
+                </div>
+            </section>
+            <div>
+                <Songs />
+            </div>
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
-import Sidebar from '../components/Sidebar'
-import Center from '../components/Center'
+import Sidebar from "../components/Sidebar";
+import Center from "../components/Center";
+import { getSession } from "next-auth/react";
 
 export default function Home() {
-  return (
-    <div className="bg-black h-screen overflow-hidden">
-      <main className='flex'>
-        <Sidebar />
-        <Center />
-        {/* Center */}
-      </main>
-      <div>
-        {/* Player */}
-      </div>
-    </div>
-  )
+    return (
+        <div className='bg-black h-screen overflow-hidden'>
+            <main className='flex'>
+                <Sidebar />
+                <Center />
+                {/* Center */}
+            </main>
+            <div>{/* Player */}</div>
+        </div>
+    );
+}
+
+export async function getServerSideProps(context) {
+    const session = await getSession(context);
+
+    return {
+        props: {
+            session,
+        },
+    };
 }
